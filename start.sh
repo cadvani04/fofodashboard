878c9a26997e5bda5eeb9d1152ef5dfd17ab8b59
Curran Advani <curranadvani@Currans-MacBook-Air.local>
2025-10-02 11:29:24 -0500

    railway compatible "

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Navigate to webapp directory
+cd webapp
+
+# Install dependencies
+npm install
+
+# Build the Next.js application
+npm run build
+
+# Start the production server
+npm start
